d133d414245372ec323ce5189df5130b066416f5
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-24 22:42:06 -0400

    updated handler slightly

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -1,24 +1,24 @@
 import { rest } from "msw";
 
 const handlers = [
-  rest.get("https://jsonplaceholder.typicode.com/posts", (req, res, ctx) => {
+  rest.get("https://jsonplaceholder.typicode.com/posts", (_, res, ctx) => {
     return res(
       ctx.json([
         {
           id: 1,
-          title: "My Post",
-          body: "This is my post",
+          title: "This is AWESOME!!!",
+          body: "It's hard to believe how simple and powerful MSW is!",
           userId: 1,
         },
       ])
     );
   }),
-  rest.get("https://jsonplaceholder.typicode.com/posts/1", (req, res, ctx) => {
+  rest.get("https://jsonplaceholder.typicode.com/posts/1", (_, res, ctx) => {
     return res(
       ctx.json({
         id: 1,
-        title: "My Post",
-        body: "This is my post",
+        title: "This is AWESOME!!!",
+        body: "It's hard to believe how simple and powerful MSW is!",
         userId: 1,
       })
     );
