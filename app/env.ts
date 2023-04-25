5ec2b523d184c31e0a0bad4e7acc871b34e56e8a
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-24 23:02:37 -0400

    schema fix up

--- app/env.ts
+++ app/env.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  MSW_ENABLED_IN_DEVELOPMENT: z.coerce.boolean().optional(),
+  MSW_ENABLED_IN_DEVELOPMENT: z
+    .enum(["true", "false"])
+    .transform((value) => value === "true")
+    .optional(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 });
 
